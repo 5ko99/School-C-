d161f24d916d6aa9c743d3da2578a756a15b5495
pepm99 <petko.krasimirov99@gmail.com>
2015-10-07 11:26:57 +0300

    1.Added app Bankomat

---
+++ Bankomat/Bankomat.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum;
+    int n=-1;
+    cout<<"Sum= ";
+    cin>>sum;
+    int m[7];
+    m[0]=1;
+    m[1]=2;
+    m[2]=5;
+    m[3]=10;
+    m[4]=20;
+    m[5]=50;
+    m[6]=100;
+    
+    for (int i=0;i<=6;i++)
+    {
+      if(sum-m[i]>=0)
+      {
+         n++;  
+      }
+    }
+    
+    while(n>=0)
+    {
+        if(sum-m[n]>=0)
+        {
+            if (sum-m[n]==0)
+            {
+                cout<<m[n];
+                return 0;
+            }
+            else
+            {
+                sum = sum-m[n];
+                cout<<m[n]<<" ";
+            }
+        }
+        else
+        {
+          n--;  
+        }
+    }
+        
+    return 0;
+}
