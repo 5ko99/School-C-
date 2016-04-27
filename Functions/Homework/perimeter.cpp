a5e18ded1a1ea9961d060aa42afa22b2e1263fe7
pepm99 <petko.krasimirov99@gmail.om>
2016-04-27 17:31:37 +0300

    1.Fixing errors in homework aps

--- Functions/Homework/perimeter.cpp
+++ Functions/Homework/perimeter.cpp
@@ -22,10 +22,12 @@ int main()
 	cin>>b;
 	cout<<"a1=";
 	cin>>a1;
-	cout<<"a1=";
-	cin>>a1;
+	cout<<"b1=";
+	cin>>b1;
 	cout<<"P="<<perimeter(a,b)-perimeter(a1,b1);
 	cout<<'\n';
+	cout<<"Lice ab="<<lice(a,b)<<endl;
+	cout<<"Lice a1b1="<<lice(a1,b1)<<endl;
 	cout<<"S="<<lice(a,b)-lice(a1,b1);
 	return 0;
 }
