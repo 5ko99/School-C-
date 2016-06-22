2281d24a2606f0fdec0bcf309407e33006786b0f
AOC <petko.krasimirov99@gmail.com>
2016-06-22 18:02:47 +0300

    Changes

--- Recursion/fibonachi.cpp
+++ Recursion/fibonachi.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int fibonachi(int n){
+long long fibonachi(int n){
     if(n<3) return 1;
     return fibonachi(n-1)+fibonachi(n-2);
 }
 
 int main()
 {
-	int n,fib;
+	int n;
+	long long fib;
 	cout<<"n=";
 	cin>>n;
 	fib=fibonachi(n);
